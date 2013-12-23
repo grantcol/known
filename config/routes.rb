fe3f5ef214c4462c0d8458913a545bf9d75300fa
Grant Collins <grantcol@usc.edu>
2013-12-22 17:43:59 -0800

    added route to users resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Known::Application.routes.draw do
   root to: 'posts#index'
   resources :posts
   resources :links
+  reousrces :users
   post 'posts/recieve'
   post 'links/recieve'
   post 'links/updateExtension'
