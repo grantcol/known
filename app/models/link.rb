e98587ec73366e98040f09f8789e696fc41e97fe
Grant Collins <grantcol@usc.edu>
2013-12-23 03:31:33 -0800

    typo fix

--- app/models/link.rb
+++ app/models/link.rb
@@ -8,7 +8,7 @@ class Link < ActiveRecord::Base
 		end.join(", ") 
 	end
 
-	def tag_list = (tags_string)
+	def tag_list=(tags_string)
 		tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
 		new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
 		self.tags = new_or_found_tags
