ddeb855c500a1ca0b628398f7f7d8a88e0a91d6d
Grant Collins <grantcol@usc.edu>
2013-12-30 22:56:43 -0800

    dropping password coulmn

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'jbuilder', '~> 1.2'
 
 gem 'rails_12factor'
 
+gem "bcrypt-ruby", :require => "bcrypt"
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Known::Application.routes.draw do
-  root to: 'posts#index'
+  get "sessions/new"
+  #root to: 'posts#index'
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  root :to => "users#new"
+  resources :users
+  resources :sessions
   resources :posts
   resources :links
   resources :users
