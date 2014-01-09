919f0f66f185b6719de0a55c2f314e4d80ce403b
Grant Collins <grantcol@usc.edu>
2014-01-09 08:10:32 -0800

    style/structure users.index 5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,9 @@ class UsersController < ApplicationController
 		@users = User.all
 	end
 
-	#def new
-		#@user = User.new
-	#end
-
-	#def create
-	#	@user = User.new(user_params)
-	#	@user.save
-	#	flash.notice = "#{@user.handle} created"
-	#	redirect_to users_path(@user)
-	#end
+	def show
+		@user = User.find(params[:id])
+	end
 
 	def new
 	  @user = User.new
