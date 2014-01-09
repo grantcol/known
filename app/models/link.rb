2058ddcecf837a03fd46661de3906f82ef3fc312
Grant Collins <grantcol@usc.edu>
2014-01-08 17:26:49 -0800

    testing

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,8 @@
 class Link < ActiveRecord::Base
 	has_many :taggings
 	has_many :tags, through: :taggings
-
+	attr_accessible :tag_list
+	
 	def tag_list
 	  self.tags.collect do |tag|
 	    tag.name
