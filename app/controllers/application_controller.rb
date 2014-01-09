387b93ebd716dd0fa6630294a05495cd32eec10a
Grant Collins <grantcol@usc.edu>
2014-01-09 14:24:04 -0800

    current_user?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,4 @@ class ApplicationController < ActionController::Base
 	@current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def current_user?(user)
-    user == current_user
-  end
-
 end
