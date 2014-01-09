4274de486c18a2afc130fefe36384ed5c4ad927e
Grant Collins <grantcol@usc.edu>
2014-01-09 06:33:59 -0800

    backend of following interaction model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
 	include UsersHelper
-	has_many :links
+	has_many :links, dependent: :destroy
+	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+	has_many :followed_users, through: :relationships, source: :followed
+	has_many :reverse_relationships, foreign_key: "followed_id",
+                                   	 class_name:  "Relationship",
+                                     dependent:   :destroy
+ 	has_many :followers, through: :reverse_relationships, source: :follower
 	has_many :members
 	has_many :groups, through: :members 
 	has_attached_file :image , #styles: { medium: "300x300>", thumb: "50x50>" },
@@ -30,5 +36,17 @@ class User < ActiveRecord::Base
 	      self.password_salt = BCrypt::Engine.generate_salt
 	      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
 	    end
+  	end
+
+  	def following?(other_user)
+	    relationships.find_by(followed_id: other_user.id)
+	end
+
+	def follow!(other_user)
+	    relationships.create!(followed_id: other_user.id)
+	end
+
+	def unfollow!(other_user)
+    	relationships.find_by(followed_id: other_user.id).destroy!
   	end
 end
---
+++ db/migrate/20140109140606_create_relationships.rb
@@ -0,0 +1,14 @@
+class CreateRelationships < ActiveRecord::Migration
+  def change
+    create_table :relationships do |t|
+      t.integer :follower_id
+      t.integer :followed_id
+      #specification column is for tracking what streams one user follows another for
+      t.string  :specification, :array = true, :default = {} 
+      t.timestamps
+    end
+    add_index :relationships, :follower_id
+    add_index :relationships, :followed_id
+    add_index :relationships, [:follower_id, :followed_id], unique: true
+  end
+end
