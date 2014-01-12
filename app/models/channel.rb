6e31ce7015a9342445d2f74971ce712cddf7fa31
Grant Collins <grantcol@usc.edu>
2014-01-12 00:32:36 -0800

    fixed attr_accessible

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -2,6 +2,6 @@ class Channel < ActiveRecord::Base
 	belongs_to :user
 	has_many :links
 	validates :user_id, presence: true
+	attr_accessible :user_id, :name, :description, :topics
 
-	attr_acessible :user_id, :name, :description, :topics
 end
