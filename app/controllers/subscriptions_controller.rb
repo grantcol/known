5cf52d68fff4efa9bd5344e1b635d393457f45b2
Grant Collins <grantcol@usc.edu>
2014-01-12 13:08:12 -0800

    fix

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,12 +1,12 @@
 class SubscriptionsController < ApplicationController
 	def create
-	    @channel = Channel.find(params[:id])
+	    @channel = Channel.find(params[:channel_id])
 	    current_user.subscribe(@channel)
 	    redirect_to @channel
   	end
 
   	def destroy
-  		@channel = Channel.find(params[:id])
+  		@channel = Channel.find(params[:channel_id])
 	    current_user.unsubscribe(@channel)
 	    redirect_to @channel
   	end
