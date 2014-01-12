93633d84f5c5ca3e3ddf0b41b56d70b06bd9b013
Grant Collins <grantcol@usc.edu>
2014-01-11 19:58:52 -0800

    redirect to the links page after voting

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,7 @@
 class Vote < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :link
-	validates :user_id, presence: true
+	#validates :user_id, presence: true
 	validates :link_id, presence: true
 
 	def upvote
