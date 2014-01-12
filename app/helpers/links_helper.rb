948c8653a548ff0abb1e5ddd16c26cb9d444a0f7
Grant Collins <grantcol@usc.edu>
2014-01-11 23:53:53 -0800

    finishing up channels -> links relations

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,5 +1,5 @@
 module LinksHelper
 	def link_params
-    	params.require(:link).permit(:title, :url, :tag_list)
+    	params.require(:link).permit(:title, :url, :tag_list, :channel_ids)
   	end
 end
